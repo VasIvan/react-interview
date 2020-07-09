b4bcf17b93bb6e48e1452dd49b5647fe0d32cf03
Vasil <iv.vasil09@gmail.com>
2020-07-09 10:40:34 +0300

    update context

--- src/contexts/TodosContext.js
+++ src/contexts/TodosContext.js
@@ -1,15 +1,18 @@
-import React, {useState, createContext} from 'react'
+import * as React from 'react'
 
-export const TodosContext = createContext()
+export const TodosContext = React.createContext()
+
+export const initialState = [
+    {id: 1, name: 'Go to the supermarket', completed: false},
+    {id: 2, name: 'Call Alice', completed: false},
+    {id: 3, name: 'Ask Alice to call Bob', completed: false},
+    {id: 4, name: 'Do the dishes', completed: false},
+    {id: 5, name: 'Change car tyres', completed: false}
+]
 
 export const TodosProvider = ({children}) => {
-    const [todos, setTodos] = useState([
-        {id: 1, name: 'Go to the supermarket', complete: false},
-        {id: 2, name: 'Call Alice', complete: false},
-        {id: 3, name: 'Ask Alice to call Bob', complete: false},
-        {id: 4, name: 'Do the dishes', complete: false},
-        {id: 5, name: 'Change car tyres', complete: false}
-    ])
+    
+    const [todos, setTodos] = React.useState(initialState)
     
     return (
         <TodosContext.Provider value={{todos, setTodos}}>
